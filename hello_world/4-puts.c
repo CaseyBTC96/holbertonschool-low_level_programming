9f8e9d15b8eec42331d2c767373f0b8803ee54b4
Casey Paul <7227@holbertonstudents.com>
2023-09-24 11:57:18 -0700

    completed task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,18 +1,16 @@
 /*
- * File: 4-puts.c
+ *File: 4-puts.c
  * Auth: Casey Paul <7227@holbertonstudents.com>
  */
-
 #include <stdio.h>
-
 /**
- * main - Prints "Programming is like building a multilingual puzzle, follwed by a new line.
+ * main - Prints "Programming is like building a multilingual
+ *		 puzzle, follwed by a new line.
  *
  * Return: always 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
