735ed4f67ae11d3790dca077e759b762a28779f4
Casey Paul <7227@holbertonstudents.com>
2023-10-02 20:45:51 -0700

    completed task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+/*
+ * File: 8-print_array.c
+ * Auth: Casey Paul
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers.
+ * @a: The array of integers.
+ * @n: The number of elementss to be printed.
+ */
+void print_array(int *a, int n)
+{
+	int index;
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", a[index]);
+
+		if (index == n - 1)
+			continue;
+
+		printf(", ");
+	}
+
+	printf("\n");
+}		
