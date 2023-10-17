bbeb8dd655e8333aafa3d042509524340b3c0916
Casey Paul <7227@holbertonstudents.com>
2023-10-16 21:04:56 -0700

    Task 0

--- static_libraries/2-strlen.c
+++ static_libraries/2-strlen.c
@@ -1,20 +1,22 @@
+/*
+ * File: 2-strlen.c
+ * Auth: Casey Paul
+ */
+
 #include "main.h"
-#include <stddef.h>
 
 /**
- * _strchr - Function that locates a character in a string
- * @s: This is the input string
- * @c: This is the character
- * Return: Return to the character found, if not return a NULL
+ * _strlen - returns the length of the string.
+ * @str: The string to get the length of.
+ *
+ * Return: The length of @str.
  */
-char *_strchr(char *s, char c)
+int  _strlen(char *s)
 {
-        for (; ; s++)
-        {
-                if (*s == c)
-                        return (s);
-                if (*s == 0)
-                        return (NULL);
-        }
-        return (NULL);
+        int len = 0;
+
+        while (*s++)
+                len++;
+
+        return (len);
 }
