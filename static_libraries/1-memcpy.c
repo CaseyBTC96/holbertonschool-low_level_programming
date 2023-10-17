3cd0f4b6971e53d70bb44113b871fd9bea2e3145
Casey Paul <7227@holbertonstudents.com>
2023-10-16 20:49:33 -0700

    Task 0

---
+++ static_libraries/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _memcpy - copies n bytes from memory area src to dest
+ *
+ * @dest: This is the output string
+ * @src: This is the input string
+ * @n: This is the number of bytess to copy
+ *
+ * Return: This is the string copied
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+        unsigned int count;
+        char *pDst = dest;
+        char const *pSrc = src;
+
+        for (count = 0; count < n; count++)
+        {
+                *pDst++ = *pSrc++;
+        }
+        return (dest);
+}
