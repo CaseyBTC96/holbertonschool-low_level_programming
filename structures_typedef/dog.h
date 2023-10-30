41ab54392f6c0662261a2ca5ba7c9a14d52a0b7d
Casey Paul <7227@holbertonstudents.com>
2023-10-30 12:02:15 -0700

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -13,4 +13,15 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+/**
+ * dog_t - typedef for strruct dog
+ */
+typedef struct dog dog_tt;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
