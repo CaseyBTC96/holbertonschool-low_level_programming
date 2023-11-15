4ec2208344035052d2b3e3d394b8d63403de7b39
Casey Paul <7227@holbertonstudents.com>
2023-11-14 18:43:38 -0800

    task 4

--- more_singly_linked_lists/4-free_listint.c
+++ more_singly_linked_lists/4-free_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * free_litint - frees a listint_t list.
+ * free_listint - frees a listint_t list.
  * @head: pointer to the list.
  **/
 void free_listint(listint_t *head)
