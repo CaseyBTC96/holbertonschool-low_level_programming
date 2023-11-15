3aae42082438e162980c037f739198d264f0239e
Casey Paul <7227@holbertonstudents.com>
2023-11-14 17:56:43 -0800

    task 3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -2,12 +2,12 @@
 
 int _strlen_recursion(char *s);
 
-/*
+/**
  * add_node_end - adds a new node at the end of a list_t list.
  * @head: pointer to the first element of the list.
  * @str: string to set in the new node.
  * Return: address of the new element, or NULL if it failed.
- */
+ **/
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new, *aux = *head;
