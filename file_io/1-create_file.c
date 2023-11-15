b1caec07545b7213e4aa281356ebb6666026a940
Casey Paul <7227@holbertonstudents.com>
2023-11-14 20:58:46 -0800

    task 1 errors

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	file_descriptor = open(filename, O_RWDR | O_CREAT | O_TRUNC, 0600)
+	file_descriptor = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600)
 		if (file_descriptor < 0)
 			return (-1);
 
