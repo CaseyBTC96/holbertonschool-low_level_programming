9f3de4dd77d80685f5e19d264b2421369c35e2c7
Casey Paul <7227@holbertonstudents.com>
2023-11-16 13:38:10 -0800

    fixed errors in task 0

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -5,7 +5,7 @@
  * @h: pointer to the list.
  * Return: number of nodes.
  **/
-size_t print_dlisstint(const dlistint_t *h)
+size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *node = h;
 	size_t cont = 0;
