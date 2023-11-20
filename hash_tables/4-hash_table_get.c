d5bde5e701804baaf5f5b74f058d9788b1a52714
Casey Paul <7227@holbertonstudents.com>
2023-11-20 12:35:49 -0800

    task 4 errors

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,21 +1,21 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - retrieves a value associatted with a key.
- * @ht: pointer  to hash table.
+ * hash_table_get - retrieves a value associated with a key.
+ * @ht: pointer to hash table.
  * @key: key.
  * Return: value associated with the key, or NULL if key couldn't be found.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
-	hash_node *temp_node;
+	hash_node_t *temp_node;
 
 	if (!ht || !key)
 		return (NULL);
 
 	/* get index */
-	index = key_index((const unsigneed char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	temp_node = ht->array[index];
 	while (temp_node)
 	{
