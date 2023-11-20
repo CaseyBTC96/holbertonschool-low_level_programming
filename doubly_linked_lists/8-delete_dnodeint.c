2f0273b7fc8e96359f298ade17bfba5892f1fa61
Casey Paul <7227@holbertonstudents.com>
2023-11-19 20:43:53 -0800

    fixed errors in task 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -10,6 +10,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *aux_node = *head;
 	dlistint_t *node_to_delete = *head;
+	unsigned int idx;
 	unsigned int cont = 0;
 
 	/* border case empty list */
